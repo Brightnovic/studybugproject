2ecc001bcc770c57981b944a9d3d9c4d6a95d84d
Brightnovic <ndububright@gmail.com>
2023-12-21 20:44:00 +0100

    setting static files11

--- studybud/settings.py
+++ studybud/settings.py
@@ -13,64 +13,56 @@ import os
 from pathlib import Path
 import dj_database_url
 import whitenoise
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-#"django-insecure-7^k9g&zpot@pdy8d$48xuljxw@t3i07b-in%hq(c$jf#2%f@k-"
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST").split(" ")
 
-
 # Application definition
-
 INSTALLED_APPS = [
-   
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-     "whitenoise.runserver_nostatic",
-       "daphne",
     "django.contrib.staticfiles",
     "base.apps.BaseConfig",
-     'livevideo',
-     'videocall',
-     'channels',
-     'privatechat',
-      
+    "livevideo",
+    "videocall",
+    "channels",
+    "privatechat",
     "rest_framework",
     "corsheaders",
 ]
 
-
 AUTH_USER_MODEL = 'base.User'
 
 ASGI_APPLICATION = "studybud.asgi.application"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.middleware.security.SecurityMiddleware",
-   
-     
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "studybud.urls"
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 TEMPLATES = [
@@ -91,10 +83,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "studybud.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -103,10 +93,9 @@ DATABASES = {
 }
 database_url = os.environ.get("DATABASE_URL")
 DATABASES["default"] = dj_database_url.parse(database_url)
-#"postgres://studybug_user:hCSWjajSWcAmeHPoyOjpwO9FupC0tpx8@dpg-cm1nm7en7f5s73enbrk0-a.oregon-postgres.render.com/studybug"
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -122,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -134,42 +122,5 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "static/"
-MEDIA_URL = '/images/'
-STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS =[
-    BASE_DIR /'static'
-]
-
-""" STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-} """
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
-MEDIA_ROOT = BASE_DIR / 'static/images'
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-  # Replace 'your_app_name' with the actual name of your app
-
-
-ASGI_APPLICATION = "studybud.asgi.application"
-
-CORS_ALLOW_ALL_ORIGINS = True
-
-
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
-}
+# https://
