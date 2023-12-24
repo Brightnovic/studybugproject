9665d3d2d3a30a2622bca2182bd6137ad82f240b
Brightnovic <ndububright@gmail.com>
2023-12-24 22:02:09 +0100

    trying normal settings!4

--- studybud/wsgi.py
+++ studybud/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 from django.core.wsgi import get_wsgi_application
  
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
 
 application = get_wsgi_application()
