528ca8585a05db1802f43ebe710776c6f7812bde
Brightnovic <ndububright@gmail.com>
2023-12-24 22:06:00 +0100

    trying normal settings!5

--- studybud/settings.py
+++ studybud/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '56456346234432rfhhnghmmhghgnf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #os.environ.get("DEBUG", "False").lower() == "true"
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studybud-9sp8.onrender.com']
 #os.environ.get("ALLOWED_HOST").split(" ")
 # Application definition
 INSTALLED_APPS = [
